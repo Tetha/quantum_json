3f46991aac8183b668f40651acc1b3a865542b16
Harald Kraemer <tetha42@gmail.com>
2014-08-04 20:18:27 +0200

    ADded Overrides

--- src/main/java/org/subquark/quantum_json/JSONBoolean.java
+++ src/main/java/org/subquark/quantum_json/JSONBoolean.java
@@ -1,6 +1,7 @@
 package org.subquark.quantum_json;
 
 public interface JSONBoolean extends ReadOnlyJSONElement {
+    @Override
     default JSONBoolean asJSONBoolean() {
         return this;
     }
--- src/main/java/org/subquark/quantum_json/JSONString.java
+++ src/main/java/org/subquark/quantum_json/JSONString.java
@@ -1,6 +1,7 @@
 package org.subquark.quantum_json;
 
 public interface JSONString extends ReadOnlyJSONElement {
+    @Override
     default JSONString asJSONString() {
         return this;
     }
