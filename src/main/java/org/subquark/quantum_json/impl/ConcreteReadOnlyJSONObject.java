ef4725a98a82b10b075ca136fa89ef0f84a3e702
Harald Kraemer <tetha42@gmail.com>
2014-08-06 19:35:19 +0200

    Added toString,equals,hashCode to ReadOnlyJSONObject

--- src/main/java/org/subquark/quantum_json/impl/ConcreteReadOnlyJSONObject.java
+++ src/main/java/org/subquark/quantum_json/impl/ConcreteReadOnlyJSONObject.java
@@ -32,5 +32,59 @@ public final class ConcreteReadOnlyJSONObject implements ReadOnlyJSONObject {
     public Map<String, ReadOnlyJSONElement> asReadOnlyJavaMap() {
         return elements; // all constructors make sure to make elements unmodifiable
     }
+
+    @Override
+    public boolean equals( Object o ) {
+        if ( o == this ) return true;
+        if ( o == null ) return false;
+
+        if ( !( o instanceof ConcreteReadOnlyJSONObject ) ) return false;
+
+        ConcreteReadOnlyJSONObject co = (ConcreteReadOnlyJSONObject) o;
+
+        if ( co.elements.size() != elements.size() ) return false;
+
+        for ( Map.Entry<String, ReadOnlyJSONElement> e : elements.entrySet() ){
+            String potentiallyCommonKey = e.getKey();
+            if ( !co.elements.containsKey( potentiallyCommonKey ) ) return false;
+
+            ReadOnlyJSONElement thisValue = e.getValue();
+            ReadOnlyJSONElement coValue = co.elements.get( potentiallyCommonKey );
+
+            if ( !thisValue.equals( coValue ) ) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int arbitraryPrime = 31;
+        int result = 1;
+        for ( Map.Entry<String, ReadOnlyJSONElement> e : elements.entrySet() ) {
+            // mostly by intuition and java best practices.. 
+            // If you have good reasons, feel free to 
+            // tweak it.
+            // if you tweaked this with good reasons,
+            // please document your reasoning here.
+            result = result * 31 + e.getKey().hashCode() + e.getValue().hashCode();
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("ConcreteReadOnlyJSONObject(");
+        result.append("elements=[");
+
+        elements.forEach( (k, v) -> result.append( k.toString() )
+                                          .append( " -> ")
+                                          .append( v.toString() ) 
+                        );
+
+        result.append("]");
+        result.append(")");
+        return result.toString();
+    }
 }
 
